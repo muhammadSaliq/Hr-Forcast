69b0ee14e9b630b73d601afde6bcb01b4b12a617
unknown <salik.rafiq11111@gmail.com>
2023-10-15 18:34:25 +0500

    first commit

--- src/components/HomeComponents/subimage/index.js
+++ src/components/HomeComponents/subimage/index.js
@@ -4,9 +4,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Button } from 'bootstrap';
 import './subimage.css'
 import Navbars from '../../navbar';
+import { useNavigate } from 'react-router';
 
 const Subimage = () => {
 
+    const navigate = useNavigate();
     return (
         <>
         <div className='images'>
@@ -14,7 +16,7 @@ const Subimage = () => {
             <div className='subdiv'>
                 <h1 className="texts">Your complete HR <br/> Management System</h1>
                 <h6 className='subtext'>Automate and simplify HR processes. Fast. <br/> Accurate. Simple to use.</h6>
-                <button className='buttonss'>Get Your Free Demo</button>
+                <button className='buttonss' onClick={() => {navigate('/signup')}}>Get Your Free Demo</button>
             </div>
         </div>
 
